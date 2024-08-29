9e8bc828771775d03fa3cb5139d1c1fe80f20a9d
yutthaphichai08 <yutthaphichaikk@gmail.com>
2024-08-29 14:31:51 +0700

    setup project

---
+++ addForm.php
@@ -0,0 +1,11 @@
+<?php
+$title = "แบบฟอร์มบันทึกข้อมูลพนักงาน";
+require_once "layout/header.php"
+?>
+
+<h1 class="text-center"><?php echo "บันทึกข้อมูลของพนักงาน"; ?></h1>
+<button class="btn btn-primary">Click</button>
+</div>
+</body>
+
+</html>
---
+++ index.php
@@ -0,0 +1,12 @@
+<?php
+$title = "หน้าแรกของเว็บไซต์";
+require_once "layout/header.php"
+?>
+
+
+<h1 class="text-center"><?php echo "ข้อมูลของพนักงาน"; ?></h1>
+<button class="btn btn-primary">Click</button>
+</div>
+</body>
+
+</html>
